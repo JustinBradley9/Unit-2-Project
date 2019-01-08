06c51122679860635237b2795acdb6bbb54b710a
Justin Bradley <justinbradley927@gmail.comgit config --global user.email justinbradley927@gmail.comgit config --global user.email justinbradley927@gmail.com>
2019-01-08 14:06:28 -0500

    creating files. created views layout and index, usercontroller and character controller,and  route index. working on getting character to show in hbs

---
+++ controller/userController.js
@@ -0,0 +1,53 @@
+User = require("../models/User")
+
+const userController = {
+    index: (req, res) => {
+        User.find({}).then(users => {
+            res.render('apps/index', { users})
+        })
+    },
+    new: (req, res) => {
+        res.render("apps/new")
+    },
+    create: (req, res) => {
+        console.log(req.body)
+        User.create({
+            name: req.body.name,
+            user: req.body.user,
+            content: req.body.content,
+            img: req.body.img,
+            createdAt: req.body.createdAt
+        }).then(newUser => {
+            res.redirect('/')
+        })
+    },
+    show: (req, res) => {
+      const UserId = req.params.id
+      User.findById(UserId).then((User) => {
+        console.log(User)
+        res.render('apps/show', { User })
+      })
+    },
+    edit: (req, res) => {
+        const UserId = req.params.id
+        // console.log(UserId)
+        res.render('apps/edit', {UserId})
+    },
+    update: (req, res) => {
+        const UserId = req.params.id
+        console.log(req.body)
+        User.findByIdAndUpdate(UserId, req.body, {new: true}).then((User) => {
+            res.redirect(`/${UserId}`)
+        })
+    },
+    delete: (req, res) => {
+        const UserId = req.params.id
+        User.findByIdAndRemove(UserId).then(() => {
+            res.redirect('/')
+        })
+    }
+}
+
+
+
+module.exports = userController
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controller/userController')
+// const characterController = require('../controller/characterController')
+
+router.get('/', userController.index)
+router.post('/', userController.create)
+router.get('/new', userController.new)
+router.get('/:id', userController.show)
+router.get('/:id/edit', userController.edit)
+router.patch('/:id', userController.update)
+router.delete('/:id', userController.delete)
+
+// router.get('/:id/character', characterController.index)
+// router.get('/:id/character/new', characterController.new)
+// router.post('/:id/character', characterController.create)
+// router.get('/:id/character/:characterId', characterController.show)
+// router.get('/:id/character/:characterId/edit', characterController.edit)
+// router.patch('/:id/character/:characterId', characterController.update)
+// router.delete('/:id/character/:characterId', characterController.delete)
+
+module.exports = router
