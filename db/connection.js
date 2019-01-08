25376a6b95bd6ba9f7ad294dd3503055bedb2230
Justin Bradley <justinbradley927@gmail.comgit config --global user.email justinbradley927@gmail.comgit config --global user.email justinbradley927@gmail.com>
2019-01-08 17:44:58 -0500

    trying to get character show page to display pt2

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost/newsLink").then(() => {
+mongoose.connect("mongodb://localhost/warteam").then(() => {
     console.log("MONGODB is now connected")
 })
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//     console.log('Connected to MongoDB')
-// }) 
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB')
+}) 
 
 
 module.exports = mongoose;
